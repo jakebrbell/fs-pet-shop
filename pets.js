db4df226511c60b4d47ce8a6db87acd2e66d4a47
Jake Bell <jakebrbell@gmail.com>
2016-06-13 15:34:02 -0700

    Add create subcommand

--- pets.js
+++ pets.js
@@ -2,14 +2,14 @@
 
 var fs = require('fs');
 var path = require('path');
-var guestsPath = path.join(__dirname, 'pets.json');
+var petsPath = path.join(__dirname, 'pets.json');
 
 var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
 
 if (cmd === 'read') {
-  fs.readFile(guestsPath, 'utf8', function(err, data) {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
     if (err) {
       throw err;
     }
@@ -28,6 +28,41 @@ if (cmd === 'read') {
     }
     else {
       console.log(pets);
+    }
+  });
+}
+else if (cmd === 'create') {
+  fs.readFile(petsPath, 'utf8', function(readErr, data) {
+    if (readErr) {
+      throw readErr;
+    }
+
+    var age = Number(process.argv[3]);
+    var kind = process.argv[4];
+    var name = process.argv[5];
+
+    if (age && kind && name) {
+      var pets = JSON.parse(data);
+      var pet = {
+        age: age,
+        kind: kind,
+        name: name
+      };
+      pets.push(pet);
+
+      var petsJSON = JSON.stringify(pets);
+
+      fs.writeFile(petsPath, petsJSON, function(writeErr) {
+        if (writeErr) {
+          throw writeErr;
+        }
+
+        console.log(pet);
+      });
+    }
+    else {
+      console.error(`Usage: ${node} ${file} create AGE KIND NAME`);
+      process.exit(1);
     }
   });
 }
