3ea9cadd2e0151efbc60aef121b81092b492dded
Jake Bell <jakebrbell@gmail.com>
2016-06-15 16:14:01 -0700

    Install and use the morgan package

--- server.js
+++ server.js
@@ -1,116 +1,14 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const petsPath = path.join(__dirname, 'pets.json');
-const fs = require('fs');
-const http = require('http');
+
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 8000;
 
-const server = http.createServer((req, res) => {
-  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-    if (err) {
-      throw err;
-    }
+app.disable('x-powered-by');
 
-    const index = Number(req.url.substr(req.url.lastIndexOf('pets/') + 5));
-    const pets = JSON.parse(petsJSON);
-    const petJSON = JSON.stringify(pets[index]);
-    const petRegExp = /^\/pets\/(.*)$/;
-
-    if (req.method === 'GET' && req.url === '/pets') {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(petsJSON);
-    }
-    else if (req.method === 'GET' && req.url.match(petRegExp) && pets[index]) {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(petJSON);
-    }
-    else if (req.method === 'POST' && req.url === '/pets') {
-      var newPet = '';
-
-      req.on('data', function(data) {
-        newPet += data.toString();
-      });
-      req.on('end', function() {
-        var newPetParsed = JSON.parse(newPet);
-        newPetParsed.age = Number(newPetParsed.age);
-        pets.push(newPetParsed);
-
-        const newPetsJSON = JSON.stringify(pets);
-
-        fs.writeFile(petsPath, newPetsJSON, (writeErr) => {
-          if (writeErr) {
-            throw writeErr;
-          }
-
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.end(newPetParsed);
-        });
-      });
-    }
-    else {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Not found');
-    }
-  });
-});
-
-server.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-
-
-
-
-
-
-
-
-// else if(req.method === 'POST' && req.url === '/pets') {
-//   let bodyJSON = '';
-//
-//   req.on('data', function(chunk) {
-//     bodyJSON += chunk.toString();
-//   });
-//
-//   req.on('end', () => {
-//     fs.readFile(petsPath, 'utf8', (readErr, data) => {
-//       if (readErr) {
-//         throw readErr;
-//       }
-//
-//       const body = JSON.parse(bodyJSON);
-//       const pets = JSON.parse(data);
-//       const age = Number.parseInt(body.age);
-//       const kind = body.kind;
-//       const name = body.name;
-//
-//       if (Number.isNaN(age) || !kind || !name) {
-//         res.statusCode = 400;
-//         res.setHeader('Content-Type', 'text/plain');
-//         res.end('Bad Request');
-//
-//         return;
-//       }
-//
-//       const pet = {age, kind, name };
-//
-//       pets.push(pet);
-//
-//       const petJSON = JSON.stringify(pet);
-//       const petsJSON = JSON.stringify(pets);
-//
-//       fs.writeFile(petsPath, petsJSON, (writeErr) => {
-//         if (writeErr) {
-//           throw writeErr;
-//         }
-//
-//         res.setHeader('Content-Type', 'application/json');
-//         res.end(petJSON);
-//       });
-//     });
-//   });
-// }
+const morgan = require('morgan');
+app.use(morgan('short'));
